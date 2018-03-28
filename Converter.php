2ff2b97ea54f9284dde939a63876521d1429fdfc
omidsaifpanahi <omid.saifpanahi1991@gmail.com>
2018-03-28 14:39:49 +0430

    Add files via upload

--- Converter.php
+++ Converter.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace omidsaifpanahi\converter;
-
+use Yii;
 /**
  * This is just an example.
  */
 class Converter extends \yii\base\Widget
 {
-    public function run()
+    public function persianDate($timestamp = null, $format = 'yyyy/MM/dd', $calenderLang = 'en-US', $calender = 'persian')
     {
-        return "Hello!";
+        if ($timestamp == null) $timestamp = time();
+        $fmt = datefmt_create($calenderLang . "@calendar=" . $calender, \IntlDateFormatter::FULL, \IntlDateFormatter::NONE, 'Asia/Tehran', \IntlDateFormatter::TRADITIONAL, $format);
+        return $fmt->format($timestamp);
     }
 }
--- README.md
+++ README.md
@@ -10,13 +10,13 @@ The preferred way to install this extension is through [composer](http://getcomp
 Either run
 
 ```
-php composer.phar require --prefer-dist omidsaifpanahi/yii2-converter "*"
+php composer.phar require --prefer-dist omidsaifpanahi/yii2-converter "dev-master"
 ```
 
 or add
 
 ```
-"omidsaifpanahi/yii2-converter": "*"
+"omidsaifpanahi/yii2-converter": "dev-master"
 ```
 
 to the require section of your `composer.json` file.
